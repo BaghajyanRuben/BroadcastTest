bd3d684e6ea0058b89dc2d6482217e7ff427a489
rubenbaghajyan <ruben.baghajyan@picsart.com>
2019-08-22 01:50:34 +0400

    improve

--- app/src/main/java/com/photo/myapplication/BootCompleteReceiver.java
+++ app/src/main/java/com/photo/myapplication/BootCompleteReceiver.java
@@ -4,24 +4,22 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.util.Log;
 import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-public class BootCompliteReciver extends BroadcastReceiver {
+public class BootCompleteReceiver extends BroadcastReceiver {
 	@Override
 	public void onReceive(Context context, Intent intent) {
 
-		Toast.makeText(context, "Device Boot complete", Toast.LENGTH_LONG).show();
+		Log.d("BootCompleteReceiver", "Device Boot complete");
 
 		Intent openIntent = new Intent(context, MainActivity.class);
 
-		openIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-
+		openIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
 
 		context.startActivity(openIntent);
-//		PendingIntent pendingIntent = PendingIntent.getActivity(context, 0,
-//				openIntent, 0);
 
 	}
 }
